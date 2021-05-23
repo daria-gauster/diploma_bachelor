58e90ade9cffe5076162e9554b4cd76976cd6acc
adamska <dooody94@gmail.com>
2021-05-23 16:18:49 +0300

    gameindo dialog

--- app/src/main/java/com/example/android/courseworkapp/DashboardActivity.kt
+++ app/src/main/java/com/example/android/courseworkapp/DashboardActivity.kt
@@ -1,21 +1,28 @@
 package com.example.android.courseworkapp
 
 import android.Manifest
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.view.Window
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.list.customListAdapter
 import com.bumptech.glide.Glide
 import com.example.android.courseworkapp.databinding.ActivityDashboardBinding
 import com.example.android.courseworkapp.model.HostedGame
 import com.example.android.courseworkapp.model.Place
+import com.google.android.material.dialog.MaterialDialogs
 import com.google.firebase.auth.FirebaseAuth
 import java.time.LocalDateTime
 
@@ -31,12 +38,17 @@ class DashboardActivity : AppCompatActivity() {
     )
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityDashboardBinding
+    private val preferences: SharedPreferences by lazy { getSharedPreferences("preferences", Context.MODE_PRIVATE) }
+    private lateinit var editor: SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //viewbinding instead of kotlin synthetics
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //initialize sharedpreferences
+        initPrefs()
 
         auth = FirebaseAuth.getInstance()
         val currentUser = auth.currentUser
@@ -56,11 +68,11 @@ class DashboardActivity : AppCompatActivity() {
 
         //load profile picture
 //        Glide.with(this).load(currentUser?.photoUrl).into(binding.include.ivProfilePic)
-//        binding.btnSignOut.setOnClickListener {
-//            auth.signOut()
-//            Intent(this, SignInActivity::class.java).also { startActivity(it) }
-//            finish()
-//        }
+        binding.btnSignOut.setOnClickListener {
+            auth.signOut()
+            Intent(this, SignInActivity::class.java).also { startActivity(it) }
+            finish()
+        }
 
         //games data
         val hostedGames = generateSampleData()
@@ -68,7 +80,7 @@ class DashboardActivity : AppCompatActivity() {
         binding.rvHostedGames.layoutManager = LinearLayoutManager(this)
         binding.rvHostedGames.adapter = GamesAdapter(this, hostedGames, object: GamesAdapter.OnClickListener {
             override fun onItemClick(position: Int) {
-                openDialog()
+                showGameinfoDialog()
             }
 
         })
@@ -90,9 +102,6 @@ class DashboardActivity : AppCompatActivity() {
 
     }
 
-    private fun openDialog() {
-        TODO("Not yet implemented")
-    }
 
     //check location permissions
     private fun checkPermission(permissionArray: Array<String>): Boolean {
@@ -123,6 +132,37 @@ class DashboardActivity : AppCompatActivity() {
         }
     }
 
+    //initialize sharedpreferences
+    private fun initPrefs() {
+        editor = preferences.edit()
+    }
+
+    private fun showGameinfoDialog() {
+
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.dialog_gameinfo)
+
+        //set default
+        dialog.findViewById<CheckBox>(R.id.cbGameParticipation).isChecked =
+            preferences.contains("cbGameParticipation") && preferences.getBoolean("cbGameParticipation",false)
+        //get new prefs
+        dialog.findViewById<Button>(R.id.btnApplyCheckBox).setOnClickListener {
+            editor.apply {
+                putBoolean(
+                    "cbGameParticipation",
+                    dialog.findViewById<CheckBox>(R.id.cbGameParticipation).isChecked
+                )
+                apply()
+            }
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    //populate games rv
     private fun generateSampleData(): List<HostedGame> {
         return listOf(
             HostedGame(
